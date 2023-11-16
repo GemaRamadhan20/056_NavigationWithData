45d55bc67e14a0d748558147564b5daebc06f584
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-11-16 17:39:30 +0700

    Add var dalam HalamanPelanggan

--- app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
@@ -2,6 +2,10 @@ package com.example.esjumbo
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -9,6 +13,10 @@ import androidx.compose.runtime.Composable
 fun CustomerDetailsScreen(
     onConfirmButtonClicked: (String, String, String) -> Unit,
     onCancelButtonClicked: () -> Unit
-){}
-class HalamanPelanggan {
+){
+    var namaPelanggan by remember { mutableStateOf("") }
+    var nomorTelepon by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("")
 }
+class HalamanPelanggan {
+}}
