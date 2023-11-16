4c0323a628a3e9c72d83a252fdc12f4f18db03f0
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-11-16 17:36:20 +0700

    Add fun CostumerDetailScreen

--- app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
@@ -1,4 +1,14 @@
 package com.example.esjumbo
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomerDetailsScreen(
+    onConfirmButtonClicked: (String, String, String) -> Unit,
+    onCancelButtonClicked: () -> Unit
+){}
 class HalamanPelanggan {
 }
