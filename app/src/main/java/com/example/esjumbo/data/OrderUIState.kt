ba4bb6dc325f24221673a3b32fe3837ffc8d5b0d
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-11-16 17:24:12 +0700

    Add vall HalamanPelanggan

--- app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
+++ app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
@@ -3,5 +3,8 @@ package com.example.esjumbo.data
 data class OrderUIState(
     val jumlah: Int = 0,
     val rasa: String = "",
-    val harga: String = ""
+    val harga: String = "",
+    val namaPelanggan: String = "",
+    val nomorTelepon: String = "",
+    val alamat: String = ""
 )
